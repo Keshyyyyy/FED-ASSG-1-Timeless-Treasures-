9e373c24774fe4df532491270752936bf60ab86f
Keshyyyyy <s10259469@connect.np.edu.sg>
2023-12-03 16:43:47 +0800

    without readme content

--- java.js
+++ java.js
@@ -1,1 +1,20 @@
+/*function for the contact page*/
+/*visual affect for the inputs in contact page*/
+const inputs = document.querySelectorAll(".input");
 
+function focusFunc(){
+    let parent = this.parentNode; /*node refers to a point in the document tree*/
+    parent.classList.add("focus"); 
+}
+
+function blurFunc(){
+    let parent = this.parentNode;
+    if(this.value==""){
+    parent.classList.remove("focus");
+}
+}
+
+inputs.forEach((input)=>{
+    input.addEventListener("focus", focusFunc);
+    input.addEventListener("blur", blurFunc);
+});
